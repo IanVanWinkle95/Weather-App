0616e2f6a35c7631a46b40cc21c3cb0c37d5c619
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-11-05 22:11:11 -0500

    added functionality

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,7 @@
 // .then(response => response.json())
 // .then(response => {
 //     console.log(response);
-    
+
 //     document.getElementById('quote').innerHTML = response.content;
 //     document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
 // })
@@ -33,18 +33,74 @@ const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
 const MY_API_KEY = "628bdc8e1ad8007298efea1d9e1f7f67"
 
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", async event => {
 
+  event.preventDefault()
+
+  const city = cityInput.value
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city)
+      displayWeatherInfo(weatherData)
+    }
+    catch (error) {
+      console.error(error)
+      displayError(error)
+    }
+  } else {
+    displayError("Enter a City")
+  }
 })
 
 async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MY_API_KEY}`
+  const response = await fetch(apiUrl)
+  // console.log(response)
 
+  if (!response.ok) {
+    throw new error("Could not fetch weather data")
+  }
+  return await response.json()
 }
 
 function displayWeatherInfo(data) {
+  // console.log(data)
+  const { name: city,
+    main: { temp, humidity },
+    weather: [{ description }] } = data
 
+  card.textContent = ""
+  card.style.display = "flex"
+
+  const cityDisplay = document.createElement("h1")
+  const tempDisplay = document.createElement("p")
+  const humidityDisplay = document.createElement("p")
+  const descriptionDisplay = document.createElement("p")
+
+  cityDisplay.textContent = city
+  tempDisplay.textContent = `${((temp - 273.15) * (9/5) + 32).toFixed(1)}°F`
+  humidityDisplay.textContent = `humidity: ${humidity}%`
+  descriptionDisplay.textContent = description
+
+  cityDisplay.classList.add("cityDisplay")
+  tempDisplay.classList.add("tempDisplay")
+  humidityDisplay.classList.add("humidityDisplay")
+  descriptionDisplay.classList.add("descrriptionDisplay")
+
+  card.appendChild(cityDisplay)
+  card.appendChild(tempDisplay)
+  card.appendChild(humidityDisplay)
+  card.appendChild(descriptionDisplay)
 }
 
 function displayError(message) {
 
+  const errorDisplay = document.createElement("p")
+
+  errorDisplay.textContent = message
+  errorDisplay.classList.add("errorDisplay")
+
+  card.textContent = ""
+  card.style.display = "flex"
+  card.appendChild(errorDisplay)
 }
